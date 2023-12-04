58ccbc833ff395507103f973679fec75c6f71d6a
maria <wi21b105@technikum-wien.at>
2023-12-04 17:57:10 +0100

    Gesamtinvestitionskosten UST implementiert + Scrollbar hinzugefügt
    Weitere TO Do's für GIK:
    -Inputvalisierung:
    Nur numerische Variablen sollten erlaubt sein;
    Bei dem Input soll 0,7 in 0.7 konvertiert werden.
    70% ebenfalls 0.7. Momentan funktioniert nur die Schreibweise 0.7
    
    Herr Wala fragen, ob Inputs so korrekt sind

--- src/main/java/com/example/financingtool/HelloApplication.java
+++ src/main/java/com/example/financingtool/HelloApplication.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
@@ -19,8 +21,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        VBox root = fxmlLoader.load();
-        Scene scene = new Scene(root, 1280, 720);
+        ScrollPane scrollPane = fxmlLoader.load();
+        VBox root = (VBox) scrollPane.getContent();
+
+        Scene scene = new Scene(scrollPane, 1280, 720);
         stage.setTitle("Hello!");
 
         resultLabel = new Label("Aktueller Wert: ");
@@ -29,13 +33,22 @@ public class HelloApplication extends Application {
         TextField[] userInputFields = new TextField[10];
         for (int i = 0; i < 10; i++) {
             userInputFields[i] = new TextField();
-            if (i<3) {
+            if (i < 3) {
                 userInputFields[i].setPromptText("KB0" + i + " netto eingeben");
-            }else{
-                userInputFields[i].setPromptText("KB0" + (i+3) + " netto eingeben");
+            } else {
+                userInputFields[i].setPromptText("KB0" + (i + 3) + " netto eingeben");
             }
 
         }
+        // Erstellen Sie Textfelder für die Benutzereingabe für Spalte D
+        TextField userInputFieldD2 = new TextField();
+        userInputFieldD2.setPromptText("UST Grund");
+
+        TextField userInputFieldD3to9 = new TextField();
+        userInputFieldD3to9.setPromptText("Genereller UST");
+
+        TextField userInputFieldD10 = new TextField();
+        userInputFieldD10.setPromptText("UST Finanzierung");
 
         // Button hinzufügen, um den Bereich von B3 bis B10 zu aktualisieren
         javafx.scene.control.Button updateRangeButton = new javafx.scene.control.Button("Bereich aktualisieren");
@@ -45,13 +58,31 @@ public class HelloApplication extends Application {
                 newValues[i] = userInputFields[i].getText();
             }
             updateRangeOfCells(newValues);
+
+        });
+        javafx.scene.control.Button updateButtonD = new javafx.scene.control.Button("UST aktualisieren");
+        updateButtonD.setOnAction(e -> {
+            String newValueD2 = userInputFieldD2.getText();
+            String newValueD3to9 = userInputFieldD3to9.getText();
+            String newValueD10 = userInputFieldD10.getText();
+
+            updateCellD(20, 1, newValueD2);
+            updateCellD(1, 3, newValueD2);
+            updateCellD(19, 1, newValueD3to9);
+            for (int i = 2; i < 9; i++) {
+                updateCellD(i, 3, newValueD3to9);
+            }
+
+            updateCellD(9, 3, newValueD10);
+
+
         });
 
         // Füge alle UI-Elemente zum Root-VBox hinzu
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             root.getChildren().add(userInputFields[i]);
         }
-        root.getChildren().addAll(resultLabel, updateRangeButton);
+        root.getChildren().addAll(resultLabel, updateRangeButton, userInputFieldD2, userInputFieldD3to9, userInputFieldD10, updateButtonD);
 
         // Setze die Szene und zeige die Bühne
         stage.setScene(scene);
@@ -61,7 +92,6 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
 
 
     private void updateRangeOfCells(String[] newValues) {
@@ -88,6 +118,7 @@ public class HelloApplication extends Application {
                 }
             }
 
+
             // Automatische Auswertung der Formeln im gesamten Arbeitsblatt
             FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
             evaluator.evaluateAll();
@@ -107,6 +138,43 @@ public class HelloApplication extends Application {
             resultLabel.setText("Fehler bei der Aktualisierung.");
         }
     }
+
+    private void updateCellD(int rowIdx, int colIdx, String newValue) {
+        try {
+            String excelFilePath = "src/main/resources/com/example/financingtool/SEPJ-Rechnungen.xlsx";
+            String sheetName = "Gesamtinvestitionskosten";
+
+            FileInputStream fileInputStream = new FileInputStream(new File(excelFilePath));
+            Workbook workbook = new XSSFWorkbook(fileInputStream);
+
+            Sheet sheet = workbook.getSheet(sheetName);
+
+            // Überprüfen Sie, ob die Zeichenkette nicht leer ist und nicht null ist, bevor Sie sie parsen
+            if (newValue != null && !newValue.isEmpty()) {
+                double newCellValue = Double.parseDouble(newValue);
+                updateCellValue(sheet, rowIdx, colIdx, newCellValue);
+            }
+
+            // Automatische Auswertung der Formeln im gesamten Arbeitsblatt
+            FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            evaluator.evaluateAll();
+
+            fileInputStream.close();
+
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(excelFilePath));
+            workbook.write(fileOutputStream);
+            fileOutputStream.close();
+            workbook.close();
+
+            resultLabel.setText("Zelle erfolgreich aktualisiert.");
+            System.out.println("Zelle erfolgreich aktualisiert.");
+
+        } catch (NumberFormatException | IOException e) {
+            e.printStackTrace();
+            resultLabel.setText("Fehler bei der Aktualisierung von Zelle D" + rowIdx);
+        }
+    }
+
 
 
     private void updateCellValue(Sheet sheet, int rowIdx, int colIdx, double newValue) {
