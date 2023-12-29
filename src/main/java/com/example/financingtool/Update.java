24bf2ef268ea44c8373f070b5efbac892c1ed157
wi21b085 <wi21b085@technikum-wien.at>
2023-12-29 05:55:48 +0100

    Validierung, String-Eingabe der Tranchen, FE korr

--- src/main/java/com/example/financingtool/Update.java
+++ src/main/java/com/example/financingtool/Update.java
@@ -65,6 +65,58 @@ public class Update {
         cell.setCellValue(newValue);
     }
 
+    public static void updateRangeOfCellsString(String[] newValues, int startRow, int endRow, int colIdx, Label resultLabel, String sheetName) {
+        try {
+            String excelFilePath = "src/main/resources/com/example/financingtool/SEPJ-Rechnungen.xlsx";
+            // String sheetName = "Gesamtinvestitionskosten";
+            //int startRow = 1;
+            //int endRow = 9;
+            //int colIdx = 1;
+
+            FileInputStream fileInputStream = new FileInputStream(new File(excelFilePath));
+            Workbook workbook = new XSSFWorkbook(fileInputStream);
+
+            Sheet sheet = workbook.getSheet(sheetName);
+
+
+            // Annahme: newValues enthält die neuen Werte für B3 bis B10
+            for (int rowIdx = startRow; rowIdx <= endRow; rowIdx++) {
+                String newValueStr = newValues[rowIdx - startRow];
+
+                // Überprüfen Sie, ob die Zeichenkette nicht leer ist und nicht null ist, bevor Sie sie parsen
+                if (newValueStr != null && !newValueStr.isEmpty()) {
+                    updateCellValueString(sheet, rowIdx, colIdx, newValueStr);
+                }
+            }
+
+
+            // Automatische Auswertung der Formeln im gesamten Arbeitsblatt
+            FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            evaluator.evaluateAll();
+
+            fileInputStream.close();
+
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(excelFilePath));
+            workbook.write(fileOutputStream);
+            fileOutputStream.close();
+            workbook.close();
+
+            resultLabel.setText("Bereich von B2 bis B10 erfolgreich aktualisiert.");
+            System.out.println("Zellen erfolgreich aktualisiert.");
+            // exportExcelToWord();
+
+        } catch (NumberFormatException | IOException e) {
+            e.printStackTrace();
+            resultLabel.setText("Fehler bei der Aktualisierung.");
+        }
+    }
+
+    public static void updateCellValueString(Sheet sheet, int rowIdx, int colIdx, String newValue) {
+        Row row = sheet.getRow(rowIdx);
+        Cell cell = row.getCell(colIdx);
+        cell.setCellValue(newValue);
+    }
+
     /*private void updateCellD(int rowIdx, int colIdx, String newValue) {
         try {
             String excelFilePath = "src/main/resources/com/example/financingtool/SEPJ-Rechnungen.xlsx";
