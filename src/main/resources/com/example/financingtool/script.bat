268c90c8cb0f8ce93698f20aa48e96e5c4d32ab2
maria <wi21b105@technikum-wien.at>
2024-01-24 21:57:08 +0100

    Batch Skript Korrektur

--- src/main/resources/com/example/financingtool/script.bat
+++ src/main/resources/com/example/financingtool/script.bat
@@ -5,51 +5,25 @@ set arg2=%2
 IF NOT EXIST "src\\main\\resources\\com\\example\\financingtool\\pre.txt" (
     echo Prerequisites not found
     echo ...
-    echo Installing Python...
+    :: Check for Python Installation
+    python --version 2>NUL
+    if errorlevel 1 goto errorNoPython
 
-    where python > nul 2>&1
+    :: Reaching here means Python is installed.
+    :: Execute stuff...
 
-    if %errorlevel% equ 0 (
-        python --version
-        echo "Python is installed"
-    ) else (
-        echo "Python is not installed"
-        winget install -e --id Python.Python.3.11 --no-upgrade --accept-package-agreements --accept-source-agreements
-        REM winget install python --no-upgrade --accept-package-agreements --accept-source-agreements
-    )
     echo ...
     echo Upgrading pip...
     python -m pip install --upgrade pip
     echo ...
     echo Installing Selenium...
-    python -c "import selenium" > nul 2>&1
-
-    if %errorlevel% equ 0 (
-        echo "Selenium is installed"
-    ) else (
-        echo "Selenium is not installed"
-        pip install --upgrade selenium
-    )
+    pip install --upgrade selenium
     echo ...
     echo Installing PIL...
-    python -c "import PIL" > nul 2>&1
-
-    if %errorlevel% equ 0 (
-        echo "Pillow (or PIL) is installed"
-    ) else (
-        echo "Pillow (or PIL) is not installed"
-        pip install --upgrade pillow
-    )
+    pip install --upgrade pillow
     echo ...
     echo Installing colorama...
-    python -c "import colorama" > nul 2>&1
-
-    if %errorlevel% equ 0 (
-        echo "Colorama is installed"
-    ) else (
-        echo "Colorama is not installed"
-        pip install --upgrade colorama
-    )
+    pip install --upgrade colorama
     echo ...
     echo Prerequisites installed> src\\main\\resources\\com\\example\\financingtool\\pre.txt
 )
@@ -57,3 +31,16 @@ echo Taking screenshot...
 python src\\main\\resources\\com\\example\\financingtool\\%arg1% %arg2%
 
 exit
+:: Once done, exit the batch file -- skips executing the errorNoPython section
+goto:eof
+
+:errorNoPython
+echo ...
+echo Error^: Python not installed
+echo ...
+echo Installing Python...
+winget install -e --id Python.Python.3.11 --no-upgrade --accept-package-agreements --accept-source-agreements
+REM winget install python --no-upgrade --accept-package-agreements --accept-source-agreements
+echo ...
+echo Please reboot your PC
+echo ...
